fa6c12f0d718e900f9ace7918fa96d89d274d8a0
Karandeep Singh <kdsingh0602@gmail.com>
2020-05-21 14:30:06 +0530

    aesthetic fixing

--- src/components/TopArtists.jsx
+++ src/components/TopArtists.jsx
@@ -16,44 +16,34 @@ function TopArtists(props){
 
     const [toggle, set] = useState(props.artist)
     const trail = useTrail(props.artists.length, {
-    config,
-    opacity: toggle ? 1 : 0,
-    x: toggle ? 0 : 20,
-    height: toggle ? 25 : 0,
-    from: { opacity: 0, x: 20, height: 0 },
-     })  
+        config,
+        opacity: toggle ? 1 : 0,
+        x: toggle ? 0 : 20,
+        height: toggle ? 25 : 0,
+        from: { opacity: 0, x: 20, height: 0 },
+    })  
 
 
     return(
     <div> 
         <Grid container direction="column" alignItems="center">
-            {/* <div style={{marginBottom: "40px"}}>
-                <ol>
-                {props.artists.map(artist => 
-                    <Grid item>
-                    <Typography color="primary">
-                        <li>{artist}</li>
-                    </Typography>
-                    </Grid>)} 
-                </ol> 
-            </div> */}
 
             <ol style={{marginBottom: "50px"}}>
                 {trail.map(({ x, height, ...rest }, index) => (
-                <Grid item>
-                    
+                    <Grid item>
+                        
                     <Typography color="primary">
-                    <li>
-                <animated.div
-                key={props.artists[index]}
-                className="trails-text"
-                style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
-                <animated.div style={{ height }}>{props.artists[index]}</animated.div>
-                </animated.div>
-                </li>
-                </Typography>
-                
-                </Grid>
+                        <li>
+                            <animated.div
+                            key={props.artists[index]}
+                            className="trails-text"
+                            style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+                            <animated.div style={{ height }}>{props.artists[index]}</animated.div>
+                            </animated.div>
+                         </li>
+                    </Typography>
+                    
+                    </Grid>
                 ))}
             </ol>       
                 
@@ -66,50 +56,50 @@ function TopArtists(props){
                 config={{duration: 1000, delay: 1200}}
                 >
                 {props => (
-                <div style={props}>
-            <Button 
-                onClick={handleClick}
-                style={{fontFamily: 'Montserrat'}}
-                variant="outlined"
-                color="primary" 
-                size="medium"
-            >
-               Get Suggestions based on your favourite artists 
-            </Button>
-            </div>
-            )}
-            </Spring>
+                    <div style={props}>
+                    <Button 
+                        onClick={handleClick}
+                        style={{fontFamily: 'Montserrat'}}
+                        variant="outlined"
+                        color="primary" 
+                        size="medium"
+                    >
+                        Get Suggestions based on your favourite artists 
+                    </Button>
+                </div>
+                )}
+                </Spring>
             :
-            <Button 
-                onClick={handleClick}
-                className={props.Classes.link}
-                variant="outlined"
-                color="primary" 
-                size="medium"
-            >
-               Hide Suggestions
-            </Button>
+                <Button 
+                    onClick={handleClick}
+                    className={props.Classes.link}
+                    variant="outlined"
+                    color="primary" 
+                    size="medium"
+                >
+                Hide Suggestions
+                </Button>
             }           
             </Grid>
         
             {trackSugg && 
             <Spring
-            from={{opacity: 0}}
-            to={{opacity: 1}}
-            config={{duration: 1200, delay: 600}}
+                from={{opacity: 0}}
+                to={{opacity: 1}}
+                config={{duration: 1200, delay: 600}}
             >
-            {prop => (
-            <div style={prop}>
-                <ul>
-                {props.suggTracks.map(track => 
-                    <Grid item >
-                    <Typography color="primary">
-                        <li>{track.name} by {track.artists["0"].name.toUpperCase()}</li>
-                    </Typography>
-                     </Grid>)}
-                </ul>
-            </div>
-            )}
+                {prop => (
+                <div style={prop}>
+                    <ul>
+                    {props.suggTracks.map(track => 
+                        <Grid item >
+                            <Typography color="primary">
+                                <li>{track.name} by {track.artists["0"].name.toUpperCase()}</li>
+                            </Typography>
+                        </Grid>)}
+                    </ul>
+                </div>
+                )}
             </Spring>}
 
         </Grid>
